b6373c1d6297a789b462bf1605b1a33ea97021ab
unknown <joaopedrolealnasci69@gmail.com>
2026-03-09 16:30:41 -0300

    console.log

---
+++ apresentacao.js
@@ -0,0 +1,3 @@
+consolo.log("Meu nome é joao ou pelo nome da prgramador que seria jayp, tenho objetivo de conquistar e resolver o mundo da programacao, tambem tenho coisas a qual gostaria de criar que sao pessoais, no caso informação confidenciaal")
+
+
